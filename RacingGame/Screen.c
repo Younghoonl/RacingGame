bafb66848ea18f75f8ee4db57eb7c82b823081c7
Younghoonl <hoon5702@naver.com>
2023-06-17 22:42:31 +0900

    Screen.c에서 GameInfoSelect()메소드 구현 ->시작 UI에서 사용자 정보 입력

--- RacingGame/Screen.c
+++ RacingGame/Screen.c
@@ -63,6 +63,109 @@ void RemoveCursor() {
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 
+int gameInfoSelect() {
+    /* 유저 이름, 자동차 선택 */
+    int x = 32;
+    int y = 8;
+    int ScreenX, ScreenY;
+    ScreenX = 4;
+    ScreenY = 1;
+
+    SetCurrentCursorPos(ScreenX, ScreenY);
+    for (int i = 0; i < 142; i++) {
+        printf("*");
+    }
+    for (int i = ScreenY + 1; i < 38; i++) {
+        SetCurrentCursorPos(ScreenX + 141, i);
+        printf("*");
+    }
+    for (int i = 143; i > 4; i--) {
+        SetCurrentCursorPos(i, 37);
+        printf("*");
+    }
+    for (int i = 37; i > ScreenY; i--) {
+        SetCurrentCursorPos(ScreenX, i);
+        printf("*");
+    }
+
+
+    SetCurrentCursorPos(41, 20);
+    for (int i = 0; i < 30; i++) {
+        printf("〓");
+    }
+
+    SetCurrentCursorPos(38, 7);
+    for (int i = 0; i < 34; i++) {
+        printf("〓");
+    }
+
+    int curPosX = 50;
+    int curPosY = 24;
+    for (y = 0; y < 5; y++) {
+        for (x = 0; x < 8; x++) {
+            SetCurrentCursorPos(curPosX + x, curPosY + y);
+            printf("%c", car[0][y][x]);
+        }
+    }
+    SetCurrentCursorPos(curPosX + x - 4, curPosY + y + 0.5);
+    printf("[1]");
+    SetCurrentCursorPos(curPosX + x - 10, curPosY + y + 2);
+    printf("속력 : 100.0km/s");
+    SetCurrentCursorPos(curPosX + x - 12, curPosY + y + 4);
+    printf("HEART : ");
+    textcolor(RED, WHITE);
+    printf("♥♥♥♥");
+    textcolor(WHITE, BLACK);
+
+
+    curPosX += 30;
+    for (y = 0; y < 5; y++) {
+        for (x = 0; x < 8; x++) {
+            SetCurrentCursorPos(curPosX + x, curPosY + y);
+            printf("%c", car[2][y][x]);
+        }
+    }
+    SetCurrentCursorPos(curPosX + x - 4, curPosY + y + 0.5);
+    printf("[2]");
+    SetCurrentCursorPos(curPosX + x - 10, curPosY + y + 2);
+    printf("속력 : 80.0km/s");
+    SetCurrentCursorPos(curPosX + x - 10, curPosY + y + 4);
+    printf("HEART : ");
+    textcolor(RED, WHITE);
+    printf("♥♥♥♥♥");
+    textcolor(WHITE, BLACK);
+
+    SetCurrentCursorPos(60, 7);
+    printf("[정보를 입력하세요!]");
+    SetCurrentCursorPos(63, 20);
+    printf("[자동차 종류]");
+    x = 38;
+    y = 11;
+    SetCurrentCursorPos(x, y);
+    printf("이름 : ");
+    SetCurrentCursorPos(x, y + 3);
+    printf("자동차 (1, 2 중 선택) : ");
+    SetCurrentCursorPos(x + 7, y);
+    scanf("%s", &userName);
+    SetCurrentCursorPos(x + 25, y + 3);
+    scanf("%d", &carNumber);
+    if (carNumber != 2) {
+        carNumber = 0;
+        heart = 4;
+        speed = 90;
+        carSpeed = 100.0;
+    }
+    else {
+        heart = 5;
+        speed = 120;
+        carSpeed = 80.0;
+    }
+    tmpHeart = heart;
+    tmpCarNumber = carNumber;
+    tmpSpeed = speed;
+    return carNumber;
+}
+
 void initScreen() {
     int ScreenX, ScreenY;
     ScreenX = 4;
