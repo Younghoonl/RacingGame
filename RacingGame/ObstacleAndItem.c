7e8a5fda9c0e32cdee5bc385decc6c69f0f15049
Younghoonl2 <dldudgns8743@gmail.com>
2023-06-18 00:25:46 +0900

    ObstacleAndItem.c를 만들고 장애물과 아이템 구조체 만들기

---
+++ RacingGame/ObstacleAndItem.c
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define BLACK 0
+#define RED 4
+#define YELLOW 14 
+#define WHITE 15 
+
+
+char Obstacles[10][9] = {
+    "еквгек",
+    "еки╕ек",
+    "еке°ек",
+    "      ",
+};
+
+extern double road[360][3];
+extern int item;
+extern bool LeftRightChange;
+extern bool using;
+bool BoostChange = false;
+bool CarChange = false;
+extern double speed;
+extern double carSpeed;
+extern int carNumber;
+extern int heart;
+extern int tmpHeart;
+extern clock_t itemTimeStart;
+extern clock_t itemTimeEnd;
+
+static int gotoxy(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos;
+    pos.Y = y;
+    pos.X = x;
+    SetConsoleCursorPosition(hConsole, pos);
+    return 0;
+}
+
+typedef struct ObstacleStruct {
+    int rN; // randomNumber
+    int x;
+}obstacle;
+
+typedef struct ItemStruct {
+    int x;
+}itemStruct;
