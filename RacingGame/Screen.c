ac55d947744abb07569e9861bf759445f59497ca
Younghoonl <hoon5702@naver.com>
2023-06-17 22:24:36 +0900

    Screen.c파일을 만들고 커서 관련 함수 추가

---
+++ RacingGame/Screen.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+
+#define GBOARD_WIDTH 150
+#define GBOARD_HEIGHT 40
+
+#define BLACK 0 
+#define BLUE 1 
+#define GREEN 2 
+#define CYAN 3 
+#define RED 4 
+#define MAGENTA 5 
+#define BROWN 6 
+#define LIGHTGRAY 7 
+#define DARKGRAY 8 
+#define LIGHTBLUE 9 
+#define LIGHTGREEN 10 
+#define LIGHTCYAN 11 
+#define LIGHTRED 12 
+#define LIGHTMAGENTA 13 
+#define YELLOW 14 
+#define WHITE 15 
+
+void SetCurrentCursorPos(int x, int y) {
+    COORD position = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
+
+COORD GetCurrentCursorPos(void) {
+    COORD curPoint;
+    CONSOLE_SCREEN_BUFFER_INFO curInfo;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+    curPoint.X = curInfo.dwCursorPosition.X;
+    curPoint.Y = curInfo.dwCursorPosition.Y;
+
+    return curPoint;
+}
